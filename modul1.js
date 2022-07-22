dc89c93c4b29eeade52ae49c9afde51e6dbef160
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-07-22 15:49:10 -0700

    Update modul1.js

--- modul1.js
+++ modul1.js
@@ -515,7 +515,82 @@
 // console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
 
 
-const users = [
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ];
+
+// const friendName = "Briana Decker";
+// const getUsersWithFriend = (users, friendName) => {
+//   // const allFriends = users.flatMap(user => user.friends);
+//  const checkFriends = users.filter(user => user.friends.includes(friendName))
+//  ;
+//  return checkFriends;
+// };
+
+// console.log(getUsersWithFriend(users, friendName));
+users = [
   {
     name: "Moore Hensley",
     email: "moorehensley@indexia.com",
@@ -580,15 +655,11 @@ const users = [
     gender: "female"
   }
 ];
-
-const friendName = "Briana Decker";
-const getUsersWithFriend = (users, friendName) => {
-  // const allFriends = users.flatMap(user => user.friends);
- const checkFriends = users.filter(user => user.allFriends === friendName)
- ;
- return checkFriends;
+const sortByName = users => {
+  const sortedName = [...users].sort((firstName, secondName) => firstName.name.localeCompare(secondName.name));
+  return sortedName;
 };
-
-console.log(getUsersWithFriend(users, friendName));
+//sortByName();
+console.log(sortByName(users));
 
 
