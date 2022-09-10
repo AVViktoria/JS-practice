732ad752a667720e75cd379082daa8d478bdb182
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-10 00:00:05 -0700

    dfdd

--- practice_Aug_26_22.js
+++ practice_Aug_26_22.js
@@ -126,3 +126,15 @@ refs.buttonAdd.addEventListener('click', onBasketButtonClick);
       
     }
 
+    import throttle from 'lodash.throttle';
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    
+    player.on('timeupdate', throttle(onPlay, 1000));
+    
+    function onPlay({ seconds }) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    }
+    
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
